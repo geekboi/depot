1c1b3a6c6056145fac7682bc2ef4407740de942c
D-Laptop1 <Laptop User@.(none)>
2012-03-05 13:04:15 -0700

    testcase commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ActiveRecord::Base
+  
+  validates :title, :description, :image_url, :presence => true
+  
+  # Be sure the price is valid 
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  
+  #check for unique title
+  validates :title, :uniqueness => true
+  
+  #valid image url
+  validates :image_url, :format => {
+    :with => %r{\.(gif|jpg|png)$}i,
+    :message => 'must be a URL for GIF, JPG or PNG image.'
+  }
+  
 end
